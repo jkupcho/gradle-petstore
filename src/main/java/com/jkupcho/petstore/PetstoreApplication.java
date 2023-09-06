3ccf8111493b27576d46ea4b87eaa2640d726e86
Jonathan <jonathan.kupcho@gmail.com>
2023-09-05 20:35:31 -0500

    chore: added basic endpoint

--- src/main/java/com/jkupcho/petstore/PetstoreApplication.java
+++ src/main/java/com/jkupcho/petstore/PetstoreApplication.java
@@ -2,10 +2,26 @@ package com.jkupcho.petstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class PetstoreApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PetstoreApplication.class, args);
     }
+
+    @RestController
+    static class EchoController {
+
+        @GetMapping("/echo")
+        public Echo getEcho(@RequestParam("message") String message) {
+            return new Echo(message);
+        }
+
+    }
+
+    record Echo(String message) {}
 }
