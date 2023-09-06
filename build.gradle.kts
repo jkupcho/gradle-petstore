d0934e065a6e5f236f18b846d35da2b1e3151b69
Jonathan <jonathan.kupcho@gmail.com>
2023-09-05 20:30:45 -0500

    chore: added baseline spring boot configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    id("java")
+    java
+    id("org.springframework.boot") version("3.1.3")
+    id("io.spring.dependency-management") version("1.1.3")
 }
 
 group = "com.jkupcho.petstore"
@@ -10,10 +12,11 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
-tasks.test {
+tasks.withType<Test> {
     useJUnitPlatform()
 }
